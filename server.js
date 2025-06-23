d9b3fc631bdf3575979dada71c221270f1a5247d
reymark <genuinepower123@gmail.com>
2025-06-24 00:53:44 +0800

    main

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 // server.js (ES module version)
 import express from 'express';
-
+import cors from 'cors'
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('hello from server');
